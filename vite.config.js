17cc82361ce40feef115733a05667eac5c2eebfe
eas0nchan <licchan@126.com>
2022-11-01 21:24:28 +0800

    init

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    outDir: './dist',
+    lib: {
+      entry: './src/index.js',
+      formats: ['es', 'cjs']
+    }
+  }
+})
